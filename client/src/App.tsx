395bc84507939ffdda246079b259daa79688bd0e
potato-lulw <om.patil17072@gmail.com>
2025-11-06 18:00:32 +0530

    CLIENT: Initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl font-bold">
+        <Button>Hello</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
